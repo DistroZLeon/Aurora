17e28b300593c3c1416001f6e2f690f8aa3b123d
Tudor Boureanu <tudor04boureanu@gmail.com>
2025-04-14 21:08:18 +0300

    What

---
+++ Aurora/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Aurora.Data;
+using Aurora.Models;
+using Aurora.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Aurora.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> Index()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        
+    }
+}
